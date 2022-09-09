a143387e4d316a83a062ee086daffb2821eff1bf
root <sambokhalidf@gmail.com>
2022-09-09 14:43:48 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *
+ * main - Every point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a == 'q' || a == 'e')
+			continue;
+	}
+	putchar(a);
+}
+putchar('\n');
+return(0)
+	}
