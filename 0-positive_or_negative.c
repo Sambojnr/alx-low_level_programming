aaeed9aab2ab1340982e57f86f09d7be5351668f
root <sambokhalidf@gmail.com>
2022-09-09 04:28:39 -0700

    task 0

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <time>
+#include <stdlib.h>
+#include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int m;
+	int n;
 
 	srand(time(0));
-	n= rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	return (0);
 }
-	
